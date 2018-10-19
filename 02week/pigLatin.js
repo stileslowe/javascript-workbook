afb21ed00b0175b575ba64ad930165432b5c0a10
Stiles Lowe <stileslowe@me.com>
2018-10-18 20:07:11 -0500

    round1 commit, will keep working

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -12,15 +12,84 @@ function pigLatin(word) {
 
   // Your code here
   
-  //did user enter a word?
-  //no: ask user to enter a word
-  //yes: identify if the first letter of the word entered is a consonant or vowel
-  //if the first letter is a vowel, add yay to the end of the word and return new word;
-  //if the first letter is a consonant, then check if the next letter is a consonant, and so forth until you find a vowel
-    //take the consonant(s) that you've found and add them to the end of the word and add 'ay' after those consonants.
+  //const translateWord = (word) => {
+
+  const wordArray = word.split('');
+  
+  
+  const vowelsArray = ['a','e','i','o','u'];
+  //const vowelsArrayLength = vowelsArray.length;
+  
+  const wordLength = wordArray.length;
+  
+  
+ 
   
 
+  const consonantArray = [];
+  let wordArrayEnd = [];
+
+  for (let step = 0; step < wordLength; step++) {
+
+    const letter = wordArray[step];
+    //console.log(step);
+    //console.log(letter + "this is the letter im on in the loop");
+    if (vowelsArray.includes(letter)) {
+      if (step == [0]) {
+        console.log(word + 'yay');
+        break;
+      } {
+          
+        //console.log(letter + " - this is the vowel");
+        //console.log(step);
+        wordArrayEnd = wordArray.slice(step);
+        //console.log(wordArrayEnd);
+        break;
+      }
+    }
+      
+    consonantArray.push(letter);
+      
+  }
+
+  /*for (let vowel = 0, vowel < vowelsArrayLength; vowel++) {
+        if (vowels.includes(letter)) {
+          console.log(letter);
+        }
+      }*/
+  //console.log(letter);
+    
+
+
+
+  //const consonant = wordArray.shift();
+  //console.log(consonant);
+    
+  //console.log(consonantArray);
+ 
+  const pigLatinArray = wordArrayEnd.concat(consonantArray + 'ay');
+  const pigLatinWord = pigLatinArray.join("");
+  console.log(pigLatinWord);
+  //console.log(wordArray);
 }
+  
+  
+  
+  
+
+
+
+//translateWord('computer');}
+//no: ask user to enter a word
+
+//yes: identify if the first letter of the word entered is a consonant or vowel
+
+//if the first letter is a vowel, add yay to the end of the word and return new word;
+//if the first letter is a consonant, then check if the next letter is a consonant, and so forth until you find a vowel
+//take the consonant(s) that you've found and add them to the end of the word and add 'ay' after those consonants.
+  
+//identifyLetter(word);
+
 
 
 function getPrompt() {
